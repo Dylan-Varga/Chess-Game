c9234eb8798bf4086aa9fa8407fb07bff636d96f
Dylan Varga <vargad@oregonstate.edu>
2023-01-31 17:28:54 -0800

    ignore

---
+++ chessGame.cpp
@@ -0,0 +1,11 @@
+#include "chessGame.h"
+#include <iostream>
+
+using namespace std;
+
+void runGame(){
+    cout << "Welcome to Chess! Press any key to continue:" << endl;
+    string userInput;
+    cin >> userInput;
+    
+}
